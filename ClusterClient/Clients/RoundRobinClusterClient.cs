930cfbbe8de006fac4f6f60acbf3aa7d47a6b50f
Dmitry_Murzin <dmitrymurzin91@gmail.com>
2022-04-04 23:59:14 +0500

    RoundRobin Client

--- ClusterClient/Clients/RoundRobinClusterClient.cs
+++ ClusterClient/Clients/RoundRobinClusterClient.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using ClusterClient.Clients;
 using log4net;
@@ -9,13 +11,49 @@ public class RoundRobinClusterClient : ClusterClientBase
 {
 	public RoundRobinClusterClient(string[] replicaAddresses)
 		: base(replicaAddresses)
-	{
-		throw new NotImplementedException();
-	}
+	{ }
 
-	public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+	public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
 	{
-		throw new NotImplementedException();
+		var badAddresses = new HashSet<string>();
+		var slowAddresses = new HashSet<string>();
+		var commonTimeout = Task.Delay(timeout);
+		var taskAverageTimeout = timeout / (ReplicaAddresses.Length - badAddresses.Count);
+		while (!commonTimeout.IsCompleted)
+		{
+			if (badAddresses.Count == ReplicaAddresses.Length)
+				throw new Exception("All replicas are bad");
+
+			foreach (var address in ReplicaAddresses)
+			{
+				if (badAddresses.Contains(address))
+					continue;
+				var sw = new Stopwatch();
+				sw.Start();
+				var timeoutTask = Task.Delay(taskAverageTimeout);
+				var webRequest = CreateRequest(address + "?query=" + query);
+				var requestTask = ProcessRequestAsync(webRequest);
+				
+				await Task.WhenAny(requestTask, timeoutTask);
+				sw.Stop();
+				if (!requestTask.IsCompleted)
+				{
+					slowAddresses.Add(address);
+					continue;
+				}
+
+				if (!requestTask.IsFaulted)
+					return requestTask.Result;
+				
+				timeout -= TimeSpan.FromMilliseconds(sw.ElapsedMilliseconds);
+				badAddresses.Add(address);
+				taskAverageTimeout = timeout / (ReplicaAddresses.Length - badAddresses.Count);
+			}
+
+			if (slowAddresses.Count == ReplicaAddresses.Length)
+				throw new TimeoutException();
+		}
+		throw new TimeoutException();
 	}
 
 	protected override ILog Log => LogManager.GetLogger(typeof(RoundRobinClusterClient));
